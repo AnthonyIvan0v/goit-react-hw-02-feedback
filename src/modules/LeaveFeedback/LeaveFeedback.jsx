97b6ce2fa999b62740335e11123b221a97258840
AnthonyIvan0v <cat.sledgehammer@gmail.com>
2023-02-08 11:01:25 +0200

    створює папку modules з файлами: LeaveFeedBack.jsx(+прописує початкову розмітку), leave-feedback.module.css(+прописує стилі)

---
+++ src/modules/LeaveFeedback/LeaveFeedback.jsx
@@ -0,0 +1,31 @@
+import { Component } from 'react';
+import styles from './leave-feedback.module.css';
+
+class LeaveFeedback extends Component {
+  render() {
+    return (
+      <div className={styles.blockStatistics}>
+        <h3 className={styles.title}>Please leave feedback</h3>
+        <button className={styles.button} type="button">
+          Good
+        </button>
+        <button className={styles.button} type="button">
+          Neutral
+        </button>
+        <button className={styles.button} type="button">
+          Bad
+        </button>
+        <div>
+          <h4>Statistics : </h4>
+          <p>Good : </p>
+          <p>Neutral : </p>
+          <p>Bad : </p>
+          <p>Total : </p>
+          <p>Posite feedback : %</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LeaveFeedback;
